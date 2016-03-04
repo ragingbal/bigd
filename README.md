ae627cb18c20298b441067a13b17c6e97eadec96
Balaji Bal <balajibal@MacBook-Pro-3.local>
2016-03-04 20:56:42 +0100

    sum python udfs

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # bigd
+
+
+
+
+Remember that transform scripts in hive should receive data from STDIN and return results to
+STDOUT.  So, to properly test  your transform script try this:
+* hive -e "select id from test limit 10" > testout.txt
+* cat testout.txt | python transform_value.py
+
+
--- python/emails.py
+++ python/emails.py
@@ -1,14 +1,18 @@
+#!/usr/bin/python
+
 import sys
 import json
 
 
 for line in sys.stdin:
-	line = line.strip()
-	t = json.loads(line)
-	#userid, movieid, rating, unixtime = line.split('\t')
-	output = []
-	person_id = str(t.get('person_id'))
-	vMap = map(str, t.get('emails'))
-	for v in vMap:
-		output.append('\t'.join([person_id,v]))
-	print '\n'.join(output)
+	try:
+		line = line.strip()
+		t = json.loads(line)
+		output = []
+		person_id = str(t.get('person_id'))
+		vMap = map(str, t.get('emails'))
+		for v in vMap:
+			output.append('\t'.join([person_id,v]))
+			print '\n'.join(output)
+	except Exception  as e:
+        pass
