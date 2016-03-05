79482319d751d4f170cd057a02f0ef94aca4a92d
Balaji Bal <balajibal@MacBook-Pro-3.local>
2016-03-05 20:38:10 +0100

    sum python udfs

--- python/friends_ids.py
+++ python/friends_ids.py
@@ -3,16 +3,15 @@ import json
 
 
 for line in sys.stdin:
-    line = line.strip()
-    t = json.loads(line)
-    #userid, movieid, rating, unixtime = line.split('\t')
-
-    #print '\t'.join([person_id,first_name,last_name])
-    #print '\n'.join(map(str, t.get('friend_ids')))
-
-    output = []
-    person_id = str(t.get('person_id'))
-    vMap = map(str, t.get('friend_ids'))
-    for v in vMap:
-        output.append('\t'.join([person_id,v]))
-    print '\n'.join(output)
+    try:
+        line = line.strip()
+        t = json.loads(line)
+        output = []
+        person_id = str(t.get('person_id'))
+        vMap = map(str, t.get('friend_ids'))
+        for v in vMap:
+            output.append('\t'.join([person_id,v]))
+    if len(output) > 0:
+        print '\n'.join(output)
+    except Exception  as e:
+        pass  
