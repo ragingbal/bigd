c70429ff1a93e694f3e861fea05d9598abad2d28
Balaji Bal <balajibal@MacBook-Pro-3.local>
2016-03-05 13:03:19 +0100

    sum python udfs

--- python/mails_sent.py
+++ python/mails_sent.py
@@ -3,13 +3,15 @@ import json
 
 
 for line in sys.stdin:
-    line = line.strip()
-    t = json.loads(line)
-
-    output = []
-    person_id = str(t.get('person_id'))
-    vMap = map(str, t.get('mails_sent'))
-    for v in vMap:
-        output.append('\t'.join([person_id,v]))
-    if len(vMap) > 0: 
-        print '\n'.join(output)
+        try:
+            line = line.strip()
+            t = json.loads(line)
+            output = []
+            person_id = str(t.get('person_id'))
+            vMap = map(str, t.get('mails_sent'))
+            for v in vMap:
+                output.append('\t'.join([person_id,v]))
+            if len(vMap) > 0: 
+                print '\n'.join(output)
+        except Exception  as e:
+            pass
