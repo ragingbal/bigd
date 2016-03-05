05912ae1e690baacf33881aa6a35cb110a452d18
Balaji Bal <balajibal@MacBook-Pro-3.local>
2016-03-06 00:33:04 +0100

    sum python udfs

--- python/esimport.py
+++ python/esimport.py
@@ -15,12 +15,3 @@ es_index = 'active_profiles'
 for line in sys.stdin:
     profile = line.strip()
     res = es.index(index="test-index", doc_type='profile', body=profile)
-
- '''
-    profiles.append(profile.decode('utf8'))
-    if len(profiles) == 1000:
-        helpers.bulk(es,profiles)
-        profiles = []
-if len(profiles) > 0:
-    helpers.bulk(es,profiles)
-'''
