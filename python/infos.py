3ee24d7e759ebdfc1bf0fa6c599fe92114790ef8
Balaji Bal <balajibal@MacBook-Pro-3.local>
2016-03-05 13:08:38 +0100

    sum python udfs

---
+++ python/infos.py
@@ -0,0 +1,17 @@
+import sys
+import json
+
+
+for line in sys.stdin:
+        try:
+            line = line.strip()
+            t = json.loads(line)
+            output = []
+            person_id = str(t.get('person_id'))
+            vMap = map(str, t.get('infos'))
+            for v in vMap:
+                output.append('\t'.join([person_id,v]))
+            if len(vMap) > 0: 
+                print '\n'.join(output)
+        except Exception  as e:
+            pass
