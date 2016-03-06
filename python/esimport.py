943ee3c2cebfa29d18734140752d3d0f5949354c
Balaji Bal <balajibal@MacBook-Pro-3.local>
2016-03-06 13:25:56 +0100

    es import

--- python/esimport.py
+++ python/esimport.py
@@ -50,7 +50,7 @@ for line in sys.stdin:
         for skip in skiplist:
             t.pop(skip, None)
         res = es.index(index="pop-profiles", doc_type='small_profile', body=t)
-        print ( '\t'.join(str(processedCount),str(skippedCount)) )
+        print ( '\t'.join([str(processedCount),str(skippedCount])) )
 
     #except Exception  as e:
         skippedCount = skippedCount + 1
