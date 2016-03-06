bc228761c92c4580430462c17181bea12edf1fa4
Balaji Bal <balajibal@MacBook-Pro-3.local>
2016-03-06 13:54:37 +0100

    es import

--- python/esimport_bulk.py
+++ python/esimport_bulk.py
@@ -44,7 +44,7 @@ actions = []
 
 for line in sys.stdin:
     
-    #try:
+    try:
         profile = line.strip()
         processedCount = processedCount + 1;
         t = json.loads(profile)
@@ -59,13 +59,13 @@ for line in sys.stdin:
         }
         actions.append(action)
 
-        if len(actions) > 10:
+        if len(actions) > 20:
             helpers.bulk(es, actions)
             actions = []
 
         print ( '\t'.join( [str(processedCount),str(skippedCount)] ))
 
-    #except Exception  as e:
+    except Exception  as e:
         skippedCount = skippedCount + 1
         pass
 
